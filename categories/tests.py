fd9e619effabba463266055b7e888357fd67c2c5
Timothy Karani <kartimothy@gmail.com>
2024-07-05 14:39:57 +0300

    #3: Testing category creation

--- categories/tests.py
+++ categories/tests.py
@@ -1,5 +1,7 @@
+import json
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 from .models import Category
 
@@ -16,3 +18,29 @@ class CategoryTests(TestCase):
     def test_category_model(self):
         self.assertEqual(self.category.user.username, "JohnDoe")
         self.assertEqual(self.category.name, "reflections")
+
+
+class CategoryViewSetTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user_1 = get_user_model().objects.create_user(
+            username="JohnDoe", password="secret"
+        )
+
+        cls.user_2 = get_user_model().objects.create_user(
+            username="JaneDoe", password="secret"
+        )
+
+    def test_can_create_category(self):
+        client = APIClient()
+        client.force_authenticate(user=self.user_1)
+        response = client.post(
+            "/api/v1/categories", {"name": "meditations"}, format="json"
+        )
+
+        self.assertEqual(response.data["name"], "meditations")
+
+        categories = Category.objects.filter(name="meditations")
+        self.assertEqual(len(categories), 1)
+        self.assertEqual(categories[0].name, "meditations")
+        self.assertEqual(categories[0].user.username, "JohnDoe")
