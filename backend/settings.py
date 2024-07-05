f61130b4511d275ebb378900e8836bbe9a34975c
Timothy Karani <kartimothy@gmail.com>
2024-07-05 18:49:58 +0300

    #7: Added Redoc and Swagger

--- backend/settings.py
+++ backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_spectacular",
     # local
     "users.apps.UsersConfig",
     "categories.apps.CategoriesConfig",
@@ -148,6 +149,13 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Jarida API",
+    "DESCRIPTION": "Part of the Jarida Project",
+    "VERSION": "1.0.0",
 }
 
 
--- users/tests.py
+++ users/tests.py
@@ -24,3 +24,48 @@ class UserAuthenticationTests(TestCase):
         self.assertEqual(response.status_code, 204)
         users = get_user_model().objects.filter(username="JohnDoe")
         self.assertEqual(len(users), 1)
+
+    def test_can_sign_in(self) -> None:
+        client = APIClient()
+        response = client.post(
+            "/api/v1/dj-rest-auth/login/",
+            data={
+                "username": "JohnDoe",
+                "password": "secret",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data["key"])
+
+    def test_can_show_user(self) -> None:
+        client = APIClient()
+        client.force_authenticate(user=self.user)
+        response = client.get("/api/v1/dj-rest-auth/user/")
+
+        self.assertEqual(response.status_code, 200)
+        print(response.data)
+        # self.assertIsNotNone(response.data["key"])
+
+    # def test_can_sign_out(self) -> None:
+    #     client = APIClient()
+    #     response = client.post(
+    #         "/api/v1/dj-rest-auth/login/",
+    #         data={
+    #             "username": "JohnDoe",
+    #             "password": "secret",
+    #         },
+    #     )
+
+    #     print(self.client)
+    #     response = client.post(
+    #         "/api/v1/dj-rest-auth/logout/",
+    #     )
+
+    #     self.assertEqual(response.status_code, 200)
+    #     # self.assertIsNotNone(response.data["key"])
+
+    #     response = client.post(
+    #         "/api/v1/dj-rest-auth/logout/",
+    #     )
+    #     # print(response.request.user)
