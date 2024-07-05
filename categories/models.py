60c09a370bf5ca3988f5a6ccfec7041d57a125a7
Timothy Karani <kartimothy@gmail.com>
2024-07-06 01:14:13 +0300

    #3: Added journal entry model

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=20)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- journal_entries/models.py
+++ journal_entries/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class JournalEntry(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "journal_entries"
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.user.username}"
