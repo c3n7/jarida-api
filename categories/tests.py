34f82d5737492b48e1df5e2dd2a787b2a1e830a3
Timothy Karani <kartimothy@gmail.com>
2024-07-05 12:52:30 +0300

    Added category model tests

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoryTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = get_user_model().objects.create_user(
+            username="JohnDoe", password="secret"
+        )
+
+        cls.category = Category.objects.create(user=cls.user, name="reflections")
+
+    def test_category_model(self):
+        self.assertEqual(self.category.user.username, "JohnDoe")
+        self.assertEqual(self.category.name, "reflections")
