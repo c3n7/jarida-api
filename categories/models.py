28abec3240ca6572156cdd7927889215568bf353
Timothy Karani <kartimothy@gmail.com>
2024-07-05 09:14:51 +0300

    #3: Added user tenancy to categories

--- categories/models.py
+++ categories/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
