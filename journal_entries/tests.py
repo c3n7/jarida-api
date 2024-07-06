6e7b8b5ce94044c34361cb0bfb6ae3195a00a369
Timothy Karani <kartimothy@gmail.com>
2024-07-06 04:20:32 +0300

    #3: testing can list journal entries

--- journal_entries/tests.py
+++ journal_entries/tests.py
@@ -33,3 +33,62 @@ class JournalEntryTests(TestCase):
         self.assertEqual(self.journal_entry.content, "Bar")
         self.assertEqual(self.journal_entry.date, "2024-07-05")
         self.assertEqual(len(self.journal_entry.categories.all()), 3)
+
+
+class JournalEntryViewSetTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user_1 = get_user_model().objects.create_user(
+            username="JohnDoe", password="secret"
+        )
+
+        cls.user_2 = get_user_model().objects.create_user(
+            username="JaneDoe", password="secret"
+        )
+
+    def test_can_list_journal_entries(self):
+        journal_1 = JournalEntry.objects.create(
+            user=self.user_1,
+            title="Foo1",
+            content="Bar1",
+            date="2024-07-01",
+        )
+        journal_2 = JournalEntry.objects.create(
+            user=self.user_1,
+            title="Foo2",
+            content="Bar2",
+            date="2024-07-02",
+        )
+        JournalEntry.objects.create(
+            user=self.user_2,
+            title="Foo3",
+            content="Bar3",
+            date="2024-07-03",
+        )
+
+        for i in range(4):
+            JournalEntryCategory.objects.create(
+                journal_entry=journal_1,
+                category=Category.objects.create(
+                    user=self.user_1, name=f"category_no_{i}"
+                ),
+            )
+        for i in range(3):
+            JournalEntryCategory.objects.create(
+                journal_entry=journal_2,
+                category=Category.objects.create(
+                    user=self.user_1, name=f"category_no_{i}"
+                ),
+            )
+
+        client = APIClient()
+        client.force_authenticate(user=self.user_1)
+        response = client.get("/api/v1/journal_entries/")
+
+        self.assertEqual(len(response.data), 2)
+
+        self.assertEqual(response.data[0]["title"], "Foo1")
+        self.assertEqual(len(response.data[0]["categories"]), 4)
+
+        self.assertEqual(response.data[1]["title"], "Foo2")
+        self.assertEqual(len(response.data[1]["categories"]), 3)
